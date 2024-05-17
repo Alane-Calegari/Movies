eb514685c918d0b2474332aa8787cb35ae0e3797
Alane Calegari <alane.calegari@simples.software>
2024-05-17 11:09:06 -0300

    Enviando projeto Movies para o GitHub

---
+++ Movies/src/test/java/com/simples/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simples.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
